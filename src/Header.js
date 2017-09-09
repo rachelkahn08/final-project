a87bfe4feb60c3cf2c0b54cdd55c10d6311edfd7
Rachel K <rkahn08@gmail.com>
2017-09-09 17:24:11 -0400

    header test
    seeing how merge works

--- src/Header.js
+++ src/Header.js
@@ -1,17 +1,38 @@
 import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
 import './App.css';
 
 class Header extends Component {
 
-  // constructor (probs) {
-  //   super(probs);
-  // }
+  constructor (probs) {
+    super(probs);
+
+    this.isLoggedIn.bind(this);
+
+    this.state = {
+    	loggedInOption: './CategoryPage',
+    }
+  }
+
+  isLoggedIn(loginState) {
+
+  	loginState = 'ImageDetail';
+
+  	this.setState({ loggedInOption: loginState });
+
+  }
 
   render() {
+  	const loggedInOption = this.state.loggedInOption;
+
     return (
-
-      <div className = "header"> I'M A HEADER</div>
-
+    	<header>
+    		<nav>
+    			<NavLink to="./CategoryPage" />
+    			<NavLink to="./ImageCollection" />
+    			<NavLink to={ loggedInOption } />
+    		</nav>
+    	</header>
     );
   }
 }
