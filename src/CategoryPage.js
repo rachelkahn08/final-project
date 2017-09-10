a0ecd7d3b62bbc1e4f6d555ae1bfe267264d854b
Rachel K <rkahn08@gmail.com>
2017-09-10 12:45:02 -0400

    re-unfucked category search

--- src/CategoryPage.js
+++ src/CategoryPage.js
@@ -13,7 +13,17 @@ class CategoryPage extends Component {
   render() {
 
     return (
-      <Gallery { queryParameters } />
+      <div>
+        <div className="CategoryPage">
+          <select className="CategoryForm">
+            <option value="value1">Value 1</option>
+            <option value="value2">Value 2</option>
+            <option value="value3">Value 3</option>
+            <option value="value4">Value 4</option>
+          </select>
+        <Gallery type='category' direction='GET' queryInputs={ this.state.queryInputs }/>
+        <Gallery type='category'/>
+      </div>
     );
   }
 }
